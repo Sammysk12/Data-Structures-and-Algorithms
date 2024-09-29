fb73c87a40ff8f75ed67ac0ba91b55360385ec11
Sammysk12 <samarthkalshetti43@gmail.com>
2024-09-29 22:31:10 +0530

    Learned about Operators in C++

--- demo.cpp
+++ demo.cpp
@@ -3,14 +3,24 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello Guys" << endl;
-    int age = 24;
-    char grade = 'A';
-    float PI = 3.14;
-    bool isSmart = true;
-    double bankBalance = 895876784;
+    // cout << "Hello Guys" << endl;
+    // int age = 24;
+    // char grade = 'A';
+    // float PI = 3.14;
+    // bool isSmart = true;
+    // double bankBalance = 895876784;
 
-    cout << age << endl; 
+    // int gradeValue = grade;
+    // cout << gradeValue << endl; 
+    
+    int age;
+    cout << "Enter the age: ";
+    cin >> age;
+
+    cout<< "Your age is : " << age << endl; 
+    return 0;
+
+
 
     return 0;
 }
---
+++ operatorsInCpp.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    //Arthematic operators
+    // int a =10; 
+    // float b=5.0;
+
+    // cout << "Sum of a and b is : " << (a+b) << endl;
+    // cout << "Diff of a and b is : " << (a-b) << endl;
+    // cout << "Product of a and b is : " << (a*b) << endl;
+    // cout << "Division of a and b is : " << (a/b) << endl;
+    
+
+    // //Relational operators
+    // cout << (3<4) << endl;
+    // cout << (3>4) << endl;
+    // cout << (3>=4) << endl;
+    // cout << (4<=4) << endl;
+    // cout << (4==4) << endl;
+    // cout << (4!=4) << endl;
+    
+
+    // //logical operators
+    // cout << !(4!=4) << endl;
+    // cout << ((4!=4) || (4!=3)) << endl;
+    // cout << ((4!=4) && (3<5))  << endl;
+
+
+    return 0;
+}

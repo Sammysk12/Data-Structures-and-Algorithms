ff0cb5aafc1f31b828eff5cf764ab7076051d409
Sammysk12 <samarthkalshetti43@gmail.com>
2024-09-29 12:44:04 +0530

    Learned about the datatypes in C++

--- demo.cpp
+++ demo.cpp
@@ -4,5 +4,13 @@ using namespace std;
 int main()
 {
     cout << "Hello Guys" << endl;
+    int age = 24;
+    char grade = 'A';
+    float PI = 3.14;
+    bool isSmart = true;
+    double bankBalance = 895876784;
+
+    cout << age << endl; 
+
     return 0;
 }
